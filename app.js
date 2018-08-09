231f6c2dcf39401567440ac771508c00addae5e8
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-09 22:26:57 +0000

    lesson 5 before merge

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ books = document.querySelectorAll('#book-list li .name');
 Array.from(books).forEach(function(book) {
     console.log(books);
 });
+
+//lesson 5
+Array.from(books).forEach(function(book){
+    book.textContent +='(book title)';
+});
+
+const bookList = document.querySelector('#book-list');
+//bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML</p>';
