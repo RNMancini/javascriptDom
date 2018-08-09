ccd9d1a1a3654c34bb6b3f1389ed6f646e8da848
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-09 22:49:34 +0000

    Lesson6

--- app.js
+++ app.js
@@ -18,3 +18,14 @@ Array.from(books).forEach(function(book){
 const bookList = document.querySelector('#book-list');
 //bookList.innerHTML = '<h2>Books and more books...</h2>';
 bookList.innerHTML += '<p>This is how you add HTML</p>';
+
+//lesson6
+
+const banner = document.querySelector('#page-banner');
+
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
+
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
