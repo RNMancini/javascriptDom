990bca26d97ccb836275afec0d8c05188eb01a7b
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-14 22:12:01 +0000

    Lesson9

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ console.log('#page-banner has child nodes:', banner.hasChildNodes());
 const clonedBanner = banner.cloneNode(true);
 console.log(clonedBanner);
 
-//lesson9
+//lesson8
 const bookList = document.querySelector('#book-list');
 
 console.log('book-list next sibling is:', bookList.nextSibling);
@@ -40,3 +40,22 @@ console.log('book-list previous sibling is:', bookList.previousSibling);
 console.log('book-list previous element sibling is:', bookList.previousElementSibling);
 
 bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!'
+
+//lesson 9
+var btns = document.querySelectorAll('#book-list .delete');
+Array.from(btns).forEach(function(btn) {
+    btn.addEventListener('click", function(e) {
+        
+        const li ') e.target.parentELement;
+        
+        li.parentNode.removeChild(li)
+});
+});
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e)){
+    e.preventDefault();
+    console.log(navigation to', e.target.textContent,'was prevented');
+    })
+}
