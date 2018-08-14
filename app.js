2d54953593a5ef6d407f1a3ac5a1f34861c57147
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-14 01:14:15 +0000

    Lesson 8

--- app.js
+++ app.js
@@ -29,3 +29,14 @@ console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
 const clonedBanner = banner.cloneNode(true);
 console.log(clonedBanner);
+
+//lesson9
+const bookList = document.querySelector('#book-list');
+
+console.log('book-list next sibling is:', bookList.nextSibling);
+console.log('book-list next element sibling is:', bookList.nextElementSibling);
+
+console.log('book-list previous sibling is:', bookList.previousSibling);
+console.log('book-list previous element sibling is:', bookList.previousElementSibling);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!'
